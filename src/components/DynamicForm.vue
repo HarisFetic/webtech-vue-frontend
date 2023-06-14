66c7d1dba05f72809ebfa359ce40c1dda3738c99
HarisFetic <fetichar@students.zhaw.ch>
2023-06-14 22:21:03 +0200

    LoadKraftuebungen

--- src/components/DynamicForm.vue
+++ src/components/DynamicForm.vue
@@ -8,95 +8,90 @@
   <div>
     <table>
       <thead>
-      <tr>
-        <th>Name</th>
-        <th>Repeat</th>
-      </tr>
+        <tr>
+          <th>Name</th>
+          <th>Repeat</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="kraftuebung in kraftuebungen" :key="kraftuebung.id">
-        <td>{{ kraftuebung.name }}</td>
-        <td>{{ kraftuebung.repeat }}</td>
-      </tr>
+        <tr v-for="kraftuebung in kraftuebungen" :key="kraftuebung.id">
+          <td>{{ kraftuebung.name }}</td>
+          <td>{{ kraftuebung.repeat }}</td>
+        </tr>
       </tbody>
     </table>
   </div>
 </template>
 
-  
-  <script>
-  export default {
-    name: 'DynamicForm',
-    props: ['title'],
-    data () {
-      return {
-        kraftuebungen: [],
-        nameField: '',
-        repeatField: '',
+<script>
+export default {
+  name: 'DynamicForm',
+  props: ['title'],
+  data() {
+    return {
+      kraftuebungen: [],
+      nameField: '',
+      repeatField: '',
+    }
+  },
+  methods: {
+    loadKraftuebungen() {
+      const endpoint = 'http://localhost:8080/kraftuebungen';
+      const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          console.log(result); // Überprüfe, ob die Daten richtig empfangen werden
+          this.kraftuebungen = result; // Setze die Liste der kraftuebungen neu
+        })
+        .catch(error => console.log('error', error));
+    },
+    save() {
+      const endpoint = 'http://localhost:8080/kraftuebungen';
+      const data = {
+        name: this.nameField,
+        repeat: this.repeatField,
+      };
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      };
+      fetch(endpoint, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+          this.nameField = ''; // Leere das Eingabefeld für den Namen
+          this.repeatField = ''; // Leere das Eingabefeld für die Wiederholungen
+          this.loadKraftuebungen(); // Lade die kraftuebungen nach dem Speichern neu
+        })
+        .catch(error => console.log('error', error));
+    },
+    async setup() {
+      if (this.$root.authenticated) {
+        this.claims = await this.$auth.getKraftuebungen();
       }
-    },
-    methods: {
-      loadKraftuebungen() {
-  const endpoint = 'http://localhost:8080/kraftuebungen';
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  fetch(endpoint, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result); // Überprüfe, ob die Daten richtig empfangen werden
-      result.forEach(kraftuebung => {
-        this.kraftuebungen.push(kraftuebung);
-        this.nameField = kraftuebung.name; // Setze den Wert von nameField
-        this.repeatField = kraftuebung.repeat; // Setze den Wert von repeatField
-      });
-    })
-    .catch(error => console.log('error', error));
-},
-      save () {
-        const endpoint = 'http://localhost:8080/kraftuebungen'
-        const data = {
-          name: this.nameField,
-          repeat: this.repeatField,
-        }
-        const requestOptions = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-            // Authorization: 'Bearer ' + this.accessToken
-          },
-          body: JSON.stringify(data)
-        }
-        fetch(endpoint, requestOptions)
-          .then(response => response.json())
-          .then(data => {
-            console.log('Success:', data)
-          })
-          .catch(error => console.log('error', error))
-      },
-      async setup () {
-        if (this.$root.authenticated) {
-          this.claims = await this.$auth.getKraftuebungen()
-          // this.accessToken = await this.$auth.getAccessToken()
-        }
-      }
-    },
-    async created () {
-      await this.setup()
-      this.loadKraftuebungen()
-    },
-    mounted () {
     }
-  }
-  </script>
-  
-  <style scoped>
-    table {
-      margin-left: auto;
-      margin-right: auto;
-    }
-    button {
-      color: blue;
-    }
-  </style>
+  },
+  async created() {
+    await this.setup();
+    this.loadKraftuebungen();
+  },
+  mounted() {}
+}
+</script>
+
+<style scoped>
+table {
+  margin-left: auto;
+  margin-right: auto;
+}
+button {
+  color: blue;
+}
+</style>
