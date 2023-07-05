507f282d7bfcdc4144aafe6889c0bd29f2d262cc
martinwipf8008 <84074157+martinwipf8008@users.noreply.github.com>
2023-07-05 15:58:46 +0200

    adding dist

---
+++ js/about.444c0965.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkforntend"]=self["webpackChunkforntend"]||[]).push([[443],{8012:function(n,e,r){r.r(e),r.d(e,{default:function(){return d}});var t=r(3396);const i={class:"about"},u=(0,t._)("h1",null,"Mithilfe dieser App können Sie ganz einfach ihre Fitness auswählen und tracken.",-1),a=(0,t._)("h2",null,"Martin Wipf & Haris Fetic",-1),s=[u,a];function c(n,e){return(0,t.wg)(),(0,t.iD)("div",i,s)}var f=r(89);const l={},h=(0,f.Z)(l,[["render",c]]);var d=h}}]);
+//# sourceMappingURL=about.444c0965.js.map
